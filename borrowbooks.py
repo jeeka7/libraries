c581a60bf6d98b0d07fed646aa215ef9d550d649
Dean Hunter <jeeka7@gmail.com>
2023-07-03 03:42:41 +0530

    Update borrowbooks.py

--- borrowbooks.py
+++ borrowbooks.py
@@ -1,2 +1,5 @@
 import streamlit as st
-st.write("hello World")
+st.write("Library management System")
+conn = st.experimental_connection('sql12629961', type='sql')
+books = conn.query('select * from library')
+st.dataframe(books)
