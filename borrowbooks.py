bbaf76fe8f2c62f6170f487ecfa4e949102c7fd6
Dean Hunter <jeeka7@gmail.com>
2023-07-03 04:49:47 +0530

    Update borrowbooks.py

--- borrowbooks.py
+++ borrowbooks.py
@@ -2,10 +2,10 @@ import streamlit as st
 st.write("Library management System")
 
 mydb=mysql.connector.connect(
-host="ip-172-31-25-66"
-user="sql12629961"
-password="C6iCpjmcus"
-database="sql12629961"
+host='ip-172-31-25-66'
+user='sql12629961'
+password='C6iCpjmcus'
+database='sql12629961'
 )
 mycursor=mydb.cursor()
 st.write("connection to free database successful")
