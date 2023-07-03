2ecda126793842ba38c352848b625ad4b19a87a8
Dean Hunter <jeeka7@gmail.com>
2023-07-03 21:47:08 +0530

    Update borrowbooks.py

--- borrowbooks.py
+++ borrowbooks.py
@@ -3,8 +3,7 @@ import mysql.connector
 st.write("Library management System")
 
 # Initialize connection.
-conn = mysql.connector.connect(host='ip-172-31-25-66',database=''sql12629961'
-' password='C6iCpjmcus',user='sql12629961')
+conn = mysql.connector.connect(host='ip-172-31-25-66',database='sql12629961',password='C6iCpjmcus',user='sql12629961')
 
 if conn.is_connected():
     st.write("Connection established")
